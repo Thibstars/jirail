309782b9d213299c45e46c47f2a50cc3750f0796
Thibstars <thibault.helsmoortel@gmail.com>
2024-07-12 16:54:59 +0200

    Add Javadoc

--- src/main/java/com/github/thibstars/jirail/helper/LanguageService.java
+++ src/main/java/com/github/thibstars/jirail/helper/LanguageService.java
@@ -3,6 +3,8 @@ package com.github.thibstars.jirail.helper;
 import java.util.Set;
 
 /**
+ * The API only supports a specific set of languages. Use this service to make sure the correct ones are used.
+ *
  * @author Thibault Helsmoortel
  */
 public interface LanguageService {
